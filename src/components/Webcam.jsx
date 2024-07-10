93f56b50871f0137d065f0eedd22a65d20009d77
Alausa <alausaabdulazeez@gmail.com>
2024-07-10 23:19:48 +0100

    used react webcam

--- src/components/Webcam.jsx
+++ src/components/Webcam.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
 import { Icon } from "@iconify/react";
-import { capture, captureLarge } from "../assets/images";
+import { capture } from "../assets/images";
 import { useDispatch, useSelector } from "react-redux";
 import { identifyObject } from "../redux/actions/objectIdentificationActions";
 import LoadingComponent from "./LoadingComponent";
 
-const Webcam = ({ handleClose }) => {
-  const videoRef = useRef(null); // Reference to the video element
+const WebcamComponent = ({ handleClose }) => {
+  const webcamRef = useRef(null); // Reference to the Webcam component
   const dispatch = useDispatch();
-  const canvasRef = useRef(null);
-  const streamRef = useRef(null); // Ref to store the media stream
   const [isOpen, setIsOpen] = useState(false);
   const [attemptedIdentification, setAttemptedIdentification] = useState(false);
 
@@ -19,13 +18,10 @@ const Webcam = ({ handleClose }) => {
   const { loading, error, item } = identifiedObjectState;
   console.log(identifiedObjectState);
 
-  // Function to capture an image from the video stream
+  // Function to capture an image from the webcam stream
   const captureImage = () => {
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    return canvas.toDataURL("image/jpeg");
+    const imageSrc = webcamRef.current.getScreenshot();
+    return imageSrc;
   };
 
   // Function to identify object
@@ -35,7 +31,7 @@ const Webcam = ({ handleClose }) => {
       setIsOpen(true); // Open the sliding component after capturing the image
       const image = captureImage();
       const blob = await (await fetch(image)).blob(); // Convert base64 to Blob
-      setCapturedImage(blob);
+      setCapturedImage(image);
 
       const formData = new FormData();
       formData.append("image", blob, "capture.jpg");
@@ -46,70 +42,35 @@ const Webcam = ({ handleClose }) => {
       console.log(error);
     }
   };
-  // End of function to identify object
-
-  // Effect to get video stream from the user's camera
-  useEffect(() => {
-    async function getMedia() {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          facingMode: { ideal: "environment" }, // Use back camera by default
-        },
-      });
-      videoRef.current.srcObject = stream; // Set the video source to the stream
-
-      streamRef.current = stream; // Store the stream in the ref
-    }
-
-    getMedia();
-
-    // Cleanup function to stop the video stream
-    return () => {
-      if (streamRef.current) {
-        streamRef.current.getTracks().forEach((track) => {
-          console.log(`Stopping track: ${track.kind}`);
-          track.stop();
-        });
-      }
-    };
-  }, []);
 
   // Function to toggle the sliding component
   const toggleSlide = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    console.log(capturedImage);
-  }, [capturedImage]);
-
   return (
-    <div className="w-full overlay flex flex-col items-center  h-full pt-8  overflow-y-auto">
+    <div className="w-full overlay flex flex-col items-center h-full pt-8 overflow-y-auto">
       <div className={`${capturedImage ? "mb-[370px]" : "mb-[70px]"} `}>
         <div className="w-[350px] h-[450px] rounded-[16px] bg-transparent backdrop-blur-lg relative">
-          <video
-            ref={videoRef}
-            autoPlay
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
             className="rounded-[16px] shadow-lg h-full w-full"
-          ></video>
+            videoConstraints={{ facingMode: { ideal: "environment" } }} // Use back camera by default
+          />
+          <img src={capturedImage} alt="" className="rounded-[16px] mt-5" />
           <div
-            className="absolute mx-auto left-[50%] translate-x-[-30px] bottom-[20px] shadow-custom-shadow-lg cursor-pointer hover:scale-110 hover:bg-black transition-all duration-300 translate-x-[calc(50% + 30px)] w-[60px] h-[60px] bg-offBlack rounded-full flex items-center justify-center"
+            className="absolute mx-auto left-[50%] translate-x-[-30px] bottom-[20px] shadow-custom-shadow-lg cursor-pointer hover:scale-110 hover:bg-black transition-all duration-300 w-[60px] h-[60px] bg-offBlack rounded-full flex items-center justify-center"
             onClick={identifyCapturedObject}
           >
-            <img src={capture} className={`w-[20px] h-[20px] `} />
+            <img src={capture} className="w-[20px] h-[20px]" />
           </div>
-
-          <canvas
-            ref={canvasRef}
-            className={`mt-[30px] rounded-md mx-auto h-[350px] w-[300px] md:w-[300px] md:h-[250px] ${
-              capturedImage ? "block" : "hidden"
-            }`}
-          ></canvas>
         </div>
       </div>
 
       <div
-        className={`  fixed bottom-0 left-0 w-full bg-white shadow-lg transition-transform transform rounded-t-2xl ${
+        className={`fixed bottom-0 left-0 w-full bg-white shadow-lg transition-transform transform rounded-t-2xl ${
           isOpen ? "translate-y-0" : "translate-y-[calc(100%-4rem)]"
         }`}
         style={{ height: "50%" }}
@@ -120,14 +81,14 @@ const Webcam = ({ handleClose }) => {
               <Icon
                 icon="eva:arrow-down-fill"
                 color="#828282"
-                className=" mx-auto w-[24px] h-[24px]"
+                className="mx-auto w-[24px] h-[24px]"
                 onClick={toggleSlide} // Toggle on click
               />
             ) : (
               <Icon
                 icon="eva:arrow-up-fill"
                 color="#828282"
-                className=" mx-auto w-[24px] h-[24px]"
+                className="mx-auto w-[24px] h-[24px]"
                 onClick={toggleSlide} // Toggle on click
               />
             )}
@@ -139,7 +100,6 @@ const Webcam = ({ handleClose }) => {
               <h2 className="text-md font-semibold m-0">No item to show</h2>
               {!loading && attemptedIdentification && item?.Message && (
                 <p className="text-center">
-                  {" "}
                   {item?.Message === "No detection" &&
                     "Couldn't identify image"}
                 </p>
@@ -155,4 +115,4 @@ const Webcam = ({ handleClose }) => {
   );
 };
 
-export default Webcam;
+export default WebcamComponent;
